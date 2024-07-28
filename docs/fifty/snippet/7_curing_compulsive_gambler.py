7a9812b97a6d65bcc39af848fb5d2689a5e9bbb3
Xiangzhuang Shen <datahonor@gmail.com>
2024-07-28 15:27:45 +0800

    fix: code snap

--- docs/fifty/snippet/7_curing_compulsive_gambler.py
+++ docs/fifty/snippet/7_curing_compulsive_gambler.py
@@ -1,5 +1,6 @@
 # --8<-- [start:solution1]
 
+
 def binomial_coefficient(n: int, k: int) -> int:
     if k > n:
         return 0
@@ -16,7 +17,12 @@ def binomial_coefficient(n: int, k: int) -> int:
 def calc_expectation() -> float:
     e = 0.0
     for i in range(36 + 1):
-        e_i = (36 * (i-1)) * binomial_coefficient(36, i) * (37/38)**(36-i) * (1/38)**i
+        e_i = (
+            (36 * (i - 1))
+            * binomial_coefficient(36, i)
+            * (37 / 38) ** (36 - i)
+            * (1 / 38) ** i
+        )
         e += e_i
     return e
 
@@ -25,13 +31,23 @@ def calc_expectation_with_bet() -> float:
     e = 0.0
     for i in range(36 + 1):
         if i == 0:
-            e_i = -56 * binomial_coefficient(36, i) * (37/38)**(36-i) * (1/38)**i
+            e_i = (
+                -56
+                * binomial_coefficient(36, i)
+                * (37 / 38) ** (36 - i)
+                * (1 / 38) ** i
+            )
         else:
-            e_i = (20 + 36 * (i-1)) * binomial_coefficient(36, i) * (37/38)**(36-i) * (1/38)**i
+            e_i = (
+                (20 + 36 * (i - 1))
+                * binomial_coefficient(36, i)
+                * (37 / 38) ** (36 - i)
+                * (1 / 38) ** i
+            )
         e += e_i
     return e
 
 
 print(f"Expectation: {calc_expectation()}\n")
 print(f"Expectation with bet: {calc_expectation_with_bet()}")
-# --8<-- [start:solution1]
+# --8<-- [end:solution1]
